9e8c1f903635bf4468c7d3be7a11f186c878f7ed
LJG-Romero <luucas.jg@gmail.com>
2021-09-22 23:34:20 -0300

    Creación proyecto React - Coder

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Coder React Proyect - Lucas Romero</h1>
+      <div className="groupInfo">
+        <p><strong>Docente: </strong>Di Salvo, Daniel</p>
+        <p><strong>Tutora: </strong>Nakaganeku, Ailín</p>
+        <p><strong>Alumno: </strong>Romero, Lucas José Gabriel</p>
+        <p><strong>Camada: </strong>RJ-19740</p>
+      </div>
     </div>
   );
 }
